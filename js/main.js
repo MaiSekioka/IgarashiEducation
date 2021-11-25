db22545a9a6a85588e036a718d5ffedb2f1748be
msekioka <m.sekioka.xx@gmail.com>
2021-11-26 06:08:29 +0900

    ヘッダー・ハンバーガーボタン・メニューのスタイル、動きの改善

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 $(function(){
-  $('#js-burgerBtn').on('click',function(){
-    $('body').addClass('is-drawerActive');
-    if($(this).attr('aria-expanded') == 'false'){
-      $(this).attr('aria-expanded',true);
+  $('#js-burgerBtn , .nav-menu-bg').on('click',function(){
+    //body　アクティブクラスの付け外し
+    $('body').toggleClass('is-drawerActive');
+    //ボタンが押され、ナビが出現するとき
+    if($('#js-burgerBtn').attr('aria-expanded') == 'false'){
+      //aria-expandedをtrueにし、ボタンのデザインを変更
+      $('#js-burgerBtn').attr('aria-expanded',true);
+      //ナビメニュー、背景を出現させる
       $('.js-globalMenu').css('visibility','visible');
-      $('.nav-menu-bg').css('visibility','visible');
-      $('#js-globalMenu').attr('aria-hidden',false);
+      $('.js-globalMenu').attr('aria-hidden',false);
     } else {
-      $(this).attr('aria-expanded',false);
+      //aria-expandedをfalseにし、ボタンのデザインを変更
+      $('#js-burgerBtn').attr('aria-expanded',false);
+      //ナビメニュー、背景を隠す
       $('.js-globalMenu').css('visibility','hidden');
-      $('.nav-menu-bg').css('visibility','hidden');
-      $('#js-globalMenu').attr('aria-hidden',true);
+      $('.js-globalMenu').attr('aria-hidden',true)
     }
   });
-  $('.nav-menu-bg').on('click',function(){
-    $('body').addClass('is-drawerActive');
-  if($('#js-burgerBtn').attr('aria-expanded') == 'false'){
-    $('#js-burgerBtn').attr('aria-expanded',true);
-  } else {
-    $('#js-burgerBtn').attr('aria-expanded',false);
-    $('.js-globalMenu').css('visibility','hidden');
-    $('.nav-menu-bg').css('visibility','hidden');
-    $('#js-globalMenu').attr('aria-hidden',true);
-  }});
 });
