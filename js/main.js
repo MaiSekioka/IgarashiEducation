a23e663f8f879e3b562fb4b1dade3eb5bf87dd49
msekioka <m.sekioka.xx@gmail.com>
2021-11-29 20:46:54 +0900

    ページ内リンク　スムーススクロールの実装

--- js/main.js
+++ js/main.js
@@ -3,18 +3,37 @@ $(function(){
     //body　アクティブクラスの付け外し
     $('body').toggleClass('is-drawerActive');
     //ボタンが押され、ナビが出現するとき
-    if($('#js-burgerBtn').attr('aria-expanded') == 'false'){
+    if($(this).attr('aria-expanded') == 'false'){
       //aria-expandedをtrueにし、ボタンのデザインを変更
-      $('#js-burgerBtn').attr('aria-expanded',true);
+      $(this).attr('aria-expanded',true);
       //ナビメニュー、背景を出現させる
       $('.js-globalMenu').css('visibility','visible');
       $('.js-globalMenu').attr('aria-hidden',false);
     } else {
       //aria-expandedをfalseにし、ボタンのデザインを変更
-      $('#js-burgerBtn').attr('aria-expanded',false);
+      $(this).attr('aria-expanded',false);
       //ナビメニュー、背景を隠す
       $('.js-globalMenu').css('visibility','hidden');
-      $('.js-globalMenu').attr('aria-hidden',true)
+      $('.js-globalMenu').attr('aria-hidden',true);
     }
+  });
+  
+  //スムーススクロール
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href = $(this).attr('href');
+    let target = $(href == '#' || href == '' ? 'html' : href);
+    let position = target.offset().top - 72;
+    $('html').animate({
+      scrollTop: position
+    },speed,'swing');
+
+    if($('body').hasClass('is-drawerActive')){
+      $('body').removeClass('is-drawerActive');
+      $('#js-burgerBtn').attr('aria-expanded',false);
+      $('.js-globalMenu').css('visibility','hidden');
+      $('.js-globalMenu').attr('aria-hidden',true);
+    }
+    return false;
   });
 });
