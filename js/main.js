6f56fb63dda82347d7fa771b04fca721020db767
msekioka <m.sekioka.xx@gmail.com>
2021-12-02 08:48:48 +0900

    スライドメニュー　背景クリックでもメニューが隠れるよう挙動を修正

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('#js-burgerBtn , .nav-menu-bg').on('click',function(){
+  $('#js-burgerBtn').on('click',function(){
     //body　アクティブクラスの付け外し
     $('body').toggleClass('is-drawerActive');
     //ボタンが押され、ナビが出現するとき
@@ -12,6 +12,25 @@ $(function(){
     } else {
       //aria-expandedをfalseにし、ボタンのデザインを変更
       $(this).attr('aria-expanded',false);
+      //ナビメニュー、背景を隠す
+      $('.js-globalMenu').css('visibility','hidden');
+      $('.js-globalMenu').attr('aria-hidden',true);
+    }
+  });
+
+  $('.nav-menu-bg').on('click',function(){
+    //body　アクティブクラスの付け外し
+    $('body').toggleClass('is-drawerActive');
+    //ボタンが押され、ナビが出現するとき
+    if($('#js-burgerBtn').attr('aria-expanded') == 'false'){
+      //aria-expandedをtrueにし、ボタンのデザインを変更
+      $('#js-burgerBtn').attr('aria-expanded',true);
+      //ナビメニュー、背景を出現させる
+      $('.js-globalMenu').css('visibility','visible');
+      $('.js-globalMenu').attr('aria-hidden',false);
+    } else {
+      //aria-expandedをfalseにし、ボタンのデザインを変更
+      $('#js-burgerBtn').attr('aria-expanded',false);
       //ナビメニュー、背景を隠す
       $('.js-globalMenu').css('visibility','hidden');
       $('.js-globalMenu').attr('aria-hidden',true);
